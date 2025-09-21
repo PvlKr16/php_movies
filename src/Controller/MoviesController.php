65f6c5425b37cfcd8ceb12c0f11b44c2604a0236
pavel <pavel_k@tut.by>
2025-09-21 17:50:57 +0300

    video 7: MoviesController edited

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -8,11 +8,12 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class MoviesController extends AbstractController
 {
-    #[Route('/movies', name: 'app_movies')]
-    public function index(): JsonResponse
+    #[Route('/movies/{name}', name: 'movies', defaults: ['name' => null], methods:['GET', 'HEAD'])]
+    public function index($name): JsonResponse
     {
         return $this->json([
-            'message' => 'Welcome to your new controller!',
+            # 'message' => 'Welcome to your new controller!',
+            'message' => $name,
             'path' => 'src/Controller/MoviesController.php',
         ]);
     }
