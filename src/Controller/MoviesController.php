424473c76e910302c868b6b26631f457918a54a0
pavel <pavel_k@tut.by>
2025-09-21 18:22:58 +0300

    video 8: MoviesController edited, index.html.twig added

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,18 +3,25 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class MoviesController extends AbstractController
+class MoviesController extends AbstractController
 {
-    #[Route('/movies/{name}', name: 'movies', defaults: ['name' => null], methods:['GET', 'HEAD'])]
-    public function index($name): JsonResponse
+//    #[Route('/movies/{name}', name: 'movies', defaults: ['name' => null], methods:['GET', 'HEAD'])]
+//    public function index($name): JsonResponse
+//    {
+//        return $this->json([
+//            # 'message' => 'Welcome to your new controller!',
+//            'message' => $name,
+//            'path' => 'src/Controller/MoviesController.php',
+//        ]);
+//    }
+    #[Route('/movies', name: 'movies')]
+    public function index(): Response
     {
-        return $this->json([
-            # 'message' => 'Welcome to your new controller!',
-            'message' => $name,
-            'path' => 'src/Controller/MoviesController.php',
+        return $this->render('index.html.twig', [
+            'title' => '',
         ]);
     }
 }
