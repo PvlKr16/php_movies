90614fc75fc9c668410519b1124e359831fd9797
pavel <pavel_k@tut.by>
2025-09-22 18:57:53 +0300

    2 routes in 1 controller Class

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -4,24 +4,25 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MoviesController extends AbstractController
 {
-//    #[Route('/movies/{name}', name: 'movies', defaults: ['name' => null], methods:['GET', 'HEAD'])]
-//    public function index($name): JsonResponse
-//    {
-//        return $this->json([
-//            # 'message' => 'Welcome to your new controller!',
-//            'message' => $name,
-//            'path' => 'src/Controller/MoviesController.php',
-//        ]);
-//    }
-    #[Route('/movies', name: 'movies')]
-    public function index(): Response
+    #[Route('/movies/{name}', name: 'movies', defaults: ['name' => null], methods:['GET', 'HEAD'])]
+    public function index($name): JsonResponse
+    {
+        return $this->json([
+            # 'message' => 'Welcome to your new controller!',
+            'message' => $name,
+            'path' => 'src/Controller/MoviesController.php',
+        ]);
+    }
+    #[Route('/movie', name: 'movie')]
+    public function movie(): Response
     {
         return $this->render('index.html.twig', [
-            'title' => '',
+            'title' => 'Inception',
         ]);
     }
 }
