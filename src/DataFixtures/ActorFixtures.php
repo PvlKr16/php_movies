5e24168c39fb89306db6b16ee455bf7c7899f505
pavel <pavel_k@tut.by>
2025-09-25 11:27:15 +0300

    fixtures fixed, ActorFixtures.php created

---
+++ src/DataFixtures/ActorFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Actor;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ActorFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $actor1 = new Actor();
+        $actor1->setName('Christian Bale');
+        $manager->persist($actor1);
+
+        $actor2 = new Actor();
+        $actor2->setName('Heath Ledger');
+        $manager->persist($actor2);
+
+        $actor3 = new Actor();
+        $actor3->setName('Robert Downey Jr');
+        $manager->persist($actor3);
+
+        $actor4 = new Actor();
+        $actor4->setName('Chris Evans');
+        $manager->persist($actor4);
+
+        $manager->flush();
+
+        $this->addReference('actor1', $actor1);
+        $this->addReference('actor2', $actor2);
+        $this->addReference('actor3', $actor3);
+        $this->addReference('actor4', $actor4);
+    }
+}
--- src/DataFixtures/MovieFixtures.php
+++ src/DataFixtures/MovieFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Actor;
 use App\Entity\Movie;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -15,6 +16,8 @@ class MovieFixtures extends Fixture
         $movie1->setDescription("The Dark Knight is a great movie");
         $movie1->setReleaseYear(2008);
         $movie1->setImagePath('https://cdn.pixabay.com/photo/2024/02/22/19/46/ai-generated-8590746_1280.png');
+        $movie1->addActor($this->getReference('actor1', Actor::class));
+        $movie1->addActor($this->getReference('actor2', Actor::class));
         $manager->persist($movie1);
 
         $movie2 = new Movie();
@@ -22,6 +25,8 @@ class MovieFixtures extends Fixture
         $movie2->setDescription("Avengers: Endgame is a great movie");
         $movie2->setReleaseYear(2019);
         $movie2->setImagePath('https://cdn.pixabay.com/photo/2024/02/22/19/46/ai-generated-8590746_1280.png');
+        $movie2->addActor($this->getReference('actor3', Actor::class));
+        $movie2->addActor($this->getReference('actor4', Actor::class));
         $manager->persist($movie2);
 
         $manager->flush();
