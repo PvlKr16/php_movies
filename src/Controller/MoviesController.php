543552f86252dbde09b7c25581d28a963fda7335
pavel <pavel_k@tut.by>
2025-09-26 14:02:31 +0300

    video 14

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -9,6 +12,11 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class MoviesController extends AbstractController
 {
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
     #[Route('/movie/{name}', name: 'movie', defaults: ['name' => null], methods:['GET', 'HEAD'])]
     public function movie($name): JsonResponse
     {
@@ -23,6 +31,14 @@ class MoviesController extends AbstractController
     {
         $movies = ["Avengers: Endgame", "Inception", "Loki", "Black Widow"];
 
+        return $this->render('index.html.twig', array('movies' => $movies));
+    }
+    #[Route('/movies_new', name: 'movies_new')]
+    public function index_new(): Response
+    {
+        $repository = $this->em->getRepository(Movie::class);
+        $movies = $repository->findAll();
+        dd($movies);
         return $this->render('index.html.twig', array('movies' => $movies));
     }
 }
